a064468959bd0f0ea5b856a725f00265f2ffdccd
Brightness0302 <brightnes0302@gmail.com>
2023-06-19 19:52:13 -0700

    update webpage with upload function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,134 @@
-import React from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { useRef, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const getAllRows = async () => {
-    let count = 0;
-    try {
-      const resCount = await axios.post('http://localhost:5000/getCount')
-      count = resCount.data.length
-      for (let index=0;index<count;index++) {
-        const resRow = await axios.post(`http://localhost:5000/getRow/${index}`)
-        console.log(resRow.data)
-      }
-    }
-    catch(err) {
-      console.log(err);
-    }
-  }
+    const [file1, setFile1] = useState("Choose File");
+    const [progress1, setProgress1] = useState(0);
+    const fileInputRef1 = useRef<HTMLInputElement>(null);
 
-  return (
-    <div className="App">
-      <button onClick={getAllRows}>Get CSV Files</button>
-    </div>
-  );
+    const [file2, setFile2] = useState("Choose File");
+    const [progress2, setProgress2] = useState(0);
+    const fileInputRef2 = useRef<HTMLInputElement>(null);
+    const getAllRows = async (type: string) => {
+        let count = 0;
+        try {
+            const resCount = await axios.post(
+                "http://localhost:5000/getCount",
+                { type: type }
+            );
+            count = resCount.data.length;
+            for (let index = 0; index < count; index++) {
+                const resRow = await axios.post(
+                    `http://localhost:5000/getRow/${index}`
+                );
+                console.log(index);
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const handleFileUpload = async (
+        event: React.ChangeEvent<HTMLInputElement>,
+        type: string
+    ) => {
+        console.log(type);
+        if (!event.target.files || event.target.files.length === 0) return;
+        const file = event.target.files[0];
+        const formData = new FormData();
+        formData.append("file", file);
+        formData.append("type", type);
+
+        if (type === "1") setFile1(file.name);
+        if (type === "2") setFile2(file.name);
+
+        const config = {
+            headers: {
+                "content-type": "multipart/form-data",
+            },
+            onUploadProgress: (progressEvent: any) => {
+                const percentage = Math.round(
+                    (progressEvent.loaded / progressEvent.total) * 100
+                );
+                if (type === "1") setProgress1(percentage);
+                else if (type === "2") setProgress2(percentage);
+            },
+        };
+
+        try {
+            const response = await axios.post(
+                "http://localhost:5000/upload",
+                formData,
+                config
+            );
+            setTimeout(function () {
+                if (type === "1") getAllRows("1");
+                if (type === "2") getAllRows("2");
+            }, 2000);
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <div className="App">
+            <div className="Row_Panel">
+                <h1>Category Export</h1>
+                <label
+                    htmlFor="file-upload1"
+                    className="custom-file-upload"
+                    onClick={() => {
+                        setProgress1(0);
+                        if (fileInputRef1.current) {
+                            fileInputRef1.current.value = "";
+                        }
+                    }}
+                >
+                    {file1}
+                </label>
+                <input
+                    id="file-upload1"
+                    ref={fileInputRef1}
+                    type="file"
+                    accept=".csv"
+                    onChange={(e) => {
+                        handleFileUpload(e, "1");
+                    }}
+                    hidden
+                />
+                <progress value={progress1} max="100" />
+            </div>
+            <div className="divider"></div>
+            <div className="Row_Panel">
+                <h1>Cost Export</h1>
+                <label
+                    htmlFor="file-upload2"
+                    className="custom-file-upload"
+                    onClick={() => {
+                        setProgress2(0);
+                        if (fileInputRef2.current) {
+                            fileInputRef2.current.value = "";
+                        }
+                    }}
+                >
+                    {file2}
+                </label>
+                <input
+                    id="file-upload2"
+                    ref={fileInputRef2}
+                    type="file"
+                    accept=".csv"
+                    onChange={(e) => {
+                        handleFileUpload(e, "2");
+                    }}
+                    hidden
+                />
+                <progress value={progress2} max="100" />
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- index.js
+++ index.js
@@ -3,16 +3,26 @@ const cors = require('cors');
 const fs = require('fs');
 const csv = require('csv-parser');
 const axios = require('axios');
+const multer = require('multer');
+const path = require('path');
 const app = express();
 const port = 5000;
 
 app.use(express.json());
 app.use(cors());
 
-const results = [];
+const results1 = [];
+const results2 = [];
 let count = 0;
 
-fs.createReadStream('sample.csv')
+const CSVTOJSON = (fileName, type) => {
+    if (type===1) {
+        results1.splice(0, results1.length)
+    }
+    if (type===2) {
+        results1.splice(0, results2.length)
+    }
+    fs.createReadStream(fileName)
     .pipe(csv())
     .on('data', (data) => {
         var arr = [];
@@ -20,33 +30,58 @@ fs.createReadStream('sample.csv')
             arr.push(data[x]);
         }
         if (!arr.includes("")) {
-            results.push({metabaseId: arr[0], siteId: arr[1], skuId: arr[2], level1: arr[3], level2: arr[4], level3: arr[5], level4: arr[6], isoCode: arr[7], lastUpdated: Date.now()});
+            if (type===1) {
+                results1.push({metabaseId: arr[0], siteId: arr[1], skuId: arr[2], level1: arr[3], level2: arr[4], level3: arr[5], level4: arr[6], isoCode: arr[7], lastUpdated: Date.now()});
+            }
+            if (type===2) {
+                results2.push({metabaseId: arr[0], siteId: arr[1], skuId: arr[2], level1: arr[3], level2: arr[4], level3: arr[5], level4: arr[6], isoCode: arr[7], lastUpdated: Date.now()});
+            }
         }
     })
     .on('end', () => {
-        // console.log(results);
+        if (type===1) {
+            console.log("1:", results1.length);
+        }
+        if (type===2) {
+            console.log("2:", results2.length);
+        }
     });
+}
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    },
+});
+
+const upload = multer({
+    storage: storage,
+});
 
 async function sendRow(arr) {
-    const res = await axios.post('https://reqres.in/api/users/', {
-        ...arr,
-    })
-    if (res.status !== 201) {
+    try {
+        const res = await axios.post('https://reqres.in/api/users/', {
+            ...arr,
+        })
+        return res.data;
+    } catch(err) {
         return null;
     }
-    return res.data;
 }
 
 app.post('/getCount', async (req, res) => {
-    res.json({length: results.length});
+    res.json({length: results1.length});
 });
 
 app.post('/getRow', async (req, res) => {
-    if (count>=results.length) {
+    if (count>=results1.length) {
         res.status(404);
         return;
     }
-    const data = await sendRow(results[count]);
+    const data = await sendRow(results1[count]);
     if (!res.headersSent) {
         res.json(data)
     }
@@ -56,10 +91,20 @@ app.post('/getRow', async (req, res) => {
 app.post('/getRow/:index', async (req, res) => {
     const index = req.params.index;
     
-    const data = await sendRow(results[index]);
+    const data = await sendRow(results1[index]);
     if (!res.headersSent) {
         res.json(data)
     }
+});
+  
+app.post('/upload', upload.single('file'), (req, res) => {
+    const { type } = req.body;
+    console.log('CSVTOJSON')
+    if (type === '1')
+        CSVTOJSON(req.file.path, 1);
+    else if (type === '2')
+        CSVTOJSON(req.file.path, 2);
+    res.send('File uploaded successfully');
 });
 
 app.listen(port, () => {
